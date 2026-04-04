3d4e21ec7168c5bc2a02dc1ae05d875c66c537dd
Chen Yang <cyishere@gmail.com>
2026-04-04 17:50:39 +0800

    Init and add config

---
+++ packages/config/prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  useTabs: false,
+  endOfLine: 'lf'
+};
+
+module.exports = config;
