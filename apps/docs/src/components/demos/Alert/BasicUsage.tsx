3997894e915f3dbe5909b7142f2491dcc246e983
Chen Yang <cyishere@gmail.com>
2026-05-06 18:31:53 +0800

    Nits

--- apps/docs/src/components/demos/Alert/BasicUsage.tsx
+++ apps/docs/src/components/demos/Alert/BasicUsage.tsx
@@ -1,25 +1,25 @@
 import { Switcher } from 'react-uikit';
 
 const BasicUsage = () => {
-	return (
-		<Switcher.Root>
-			<Switcher.List className="uk-tab">
-				<Switcher.Trigger>Preview</Switcher.Trigger>
-				<Switcher.Trigger>Code</Switcher.Trigger>
-			</Switcher.List>
+  return (
+    <Switcher.Root>
+      <Switcher.List className="uk-tab">
+        <Switcher.Trigger>Preview</Switcher.Trigger>
+        <Switcher.Trigger>Code</Switcher.Trigger>
+      </Switcher.List>
 
-			<Switcher.Container className="uk-margin">
-				<Switcher.Panel>
-					<div className="uk-card uk-card-default uk-card-body">
-						<p>Component preview</p>
-					</div>
-				</Switcher.Panel>
-				<Switcher.Panel>
-					<p>code example</p>
-				</Switcher.Panel>
-			</Switcher.Container>
-		</Switcher.Root>
-	);
+      <Switcher.Container className="uk-margin">
+        <Switcher.Panel>
+          <div className="uk-card uk-card-default uk-card-body">
+            <p>Component preview</p>
+          </div>
+        </Switcher.Panel>
+        <Switcher.Panel>
+          <p>code example</p>
+        </Switcher.Panel>
+      </Switcher.Container>
+    </Switcher.Root>
+  );
 };
 
 export default BasicUsage;
