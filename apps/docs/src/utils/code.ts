ecb3c5c448ee3a29b3713e71fd46011bc7d525c8
Chen Yang <cyishere@gmail.com>
2026-05-12 17:04:04 +0800

    Nit

--- apps/docs/src/utils/code.ts
+++ apps/docs/src/utils/code.ts
@@ -1,14 +1,17 @@
-export function extractComponentCode(rawCode: string, componentName: string = 'Accordion.Root'): string {
+export const extractComponentCode = (
+  rawCode: string,
+  componentName: string = 'Accordion.Root'
+): string => {
   // Escape the component name to be used in regex
   const tag = componentName.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-  
+
   const regex = new RegExp(`( *)<${tag}[\\s\\S]*<\\/${tag}>`);
   const match = rawCode.match(regex);
-  
+
   if (match) {
     const leadingSpaces = match[1];
     return match[0].replace(new RegExp('\\n' + leadingSpaces, 'g'), '\n').trim();
   }
-  
+
   return rawCode;
-}
+};
