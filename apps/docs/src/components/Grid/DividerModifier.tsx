0054827a63b0f90fd057a6439dd16c6f4e5b42f4
Chen Yang <cyishere@gmail.com>
2026-05-21 15:41:24 +0800

    docs: update documentation for Grid

---
+++ apps/docs/src/components/Grid/DividerModifier.tsx
@@ -0,0 +1,22 @@
+import { Grid } from 'react-uikit';
+
+const DividerModifier = () => {
+  return (
+    <Grid className="uk-grid-divider uk-child-width-expand@s">
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.
+      </div>
+      <div>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </div>
+      <div>
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+      </div>
+    </Grid>
+  );
+};
+
+export default DividerModifier;
---
+++ apps/docs/src/components/Grid/GapModifier.tsx
@@ -0,0 +1,57 @@
+import { Grid } from 'react-uikit';
+
+const GapModifier = () => {
+  return (
+    <div>
+      <Grid className="uk-grid-small uk-child-width-expand@s uk-text-center">
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+      </Grid>
+
+      <Grid className="uk-grid-medium uk-child-width-expand@s uk-text-center">
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+      </Grid>
+
+      <Grid className="uk-grid-large uk-child-width-expand@s uk-text-center">
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+        <div>
+          <div className="uk-card uk-card-default uk-card-body">Item</div>
+        </div>
+      </Grid>
+
+      <Grid className="uk-grid-collapse uk-child-width-expand@s uk-text-center uk-margin-large-top">
+        <div>
+          <div className="uk-background-muted uk-padding">Item</div>
+        </div>
+        <div>
+          <div className="uk-background-primary uk-padding uk-light">Item</div>
+        </div>
+        <div>
+          <div className="uk-background-secondary uk-padding uk-light">Item</div>
+        </div>
+      </Grid>
+    </div>
+  );
+};
+
+export default GapModifier;
