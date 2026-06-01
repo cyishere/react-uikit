9b19a1ffd50612ebd24d7a2f07d27ad0fb6544f1
CY <cyishere@gmail.com>
2026-06-01 16:09:37 +0800

    feat: add prev and next links for docs (#17)

--- apps/docs/src/utils/content.ts
+++ apps/docs/src/utils/content.ts
@@ -1,33 +1,66 @@
 import type { DocsCategory } from '../content.config';
-import type { Docs, DocsForNav } from '@/utils/types';
+import type { Docs, NavItem } from '@/utils/types';
 
 import { getCollection } from 'astro:content';
 
 export const getAllDocs = async () => {
-  return (await getCollection('docs')) as Docs[];
+  const allDocs = (await getCollection('docs')) as Docs[];
+  let docsInGettingStarted: Docs[] = [];
+  const docsInComponents: Docs[] = [];
+
+  allDocs.forEach((d) => {
+    if (d.data.category === 'getting_started') {
+      docsInGettingStarted.push(d);
+    }
+
+    if (d.data.category === 'components') {
+      docsInComponents.push(d);
+    }
+  });
+
+  docsInGettingStarted = docsInGettingStarted.sort(
+    (a, b) => (a.data.order ?? 0) - (b.data.order ?? 0)
+  );
+
+  return [...docsInGettingStarted, ...docsInComponents];
 };
 
-export const getDocsForNav = async (): Promise<DocsForNav[]> => {
-  const docs = (await getCollection('docs')) as Docs[];
-
-  return docs.map((d) => ({
-    id: d.id,
-    title: d.data.title,
-    navTitle: d.data.navTitle,
-    category: d.data.category,
-    order: d.data.order
-  }));
+export const getSlug = (id: string) => {
+  return id === 'introduction' ? '' : id;
 };
 
-export const getDocsForNavByCategory = (docs: DocsForNav[], category: DocsCategory) => {
-  let fileterd = docs.filter((d) => d.category === category);
+export const getNavItem = (doc: Docs): NavItem => {
+  return {
+    slug: getSlug(doc.id),
+    label: doc.data.navTitle || doc.data.title
+  };
+};
 
-  if (category === 'getting_started') {
-    fileterd = fileterd.sort((a, b) => (a.order ?? 0) - (b.order ?? 0));
+export const getDocsForNavByCategory = async (category: DocsCategory): Promise<NavItem[]> => {
+  const allDocs = await getAllDocs();
+
+  const fileterd = allDocs.filter((d) => d.data.category === category);
+
+  return fileterd.map((d) => getNavItem(d));
+};
+
+export const getPrevAndNext = async (
+  slug: string
+): Promise<{ prev: NavItem | null; next: NavItem | null }> => {
+  const allDocs = await getAllDocs();
+  const currentIndex = allDocs.findIndex((d) => d.id === slug);
+  let prev: NavItem | null = null;
+  let next: NavItem | null = null;
+
+  if (currentIndex !== 0) {
+    const prevDoc = allDocs[currentIndex - 1];
+    prev = getNavItem(prevDoc);
   }
 
-  return fileterd.map((d) => ({
-    label: d.navTitle || d.title,
-    slug: d.id === 'introduction' ? '' : d.id
-  }));
+  if (currentIndex !== allDocs.length - 1) {
+    const nextDoc = allDocs[currentIndex + 1];
+    next = getNavItem(nextDoc);
+  }
+
+  return { prev, next };
 };
